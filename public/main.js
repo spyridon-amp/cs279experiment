ad51b42eb550b5701a67cb7576ff7626d20e301e
spyridon-amp <sampanavos@gsd.harvard.edu>
2017-09-21 01:56:42 -0400

    locating canvas elements

--- public/main.js
+++ public/main.js
@@ -5,7 +5,8 @@ var coordsTrack = [];
 
 function Login() {
     userName = document.getElementById("userName").value;
-    $.ajax({url: "/user",
+    $.ajax({
+            url: "/user",
             type: "POST",
             dataType: "json",
             data: JSON.stringify({user: userName}),
@@ -32,8 +33,13 @@ function Login() {
 }
 
 function Mainonload() {
+    draw();
+    drawtop();
+
     var central = document.getElementById("central");
     var topBarRect = document.getElementById("topBar").getBoundingClientRect();
+    var displayBar = document.getElementById("displayBar");
+    var displayRect = displayBar.getBoundingClientRect();
     central.style.top = topBarRect.top + topBarRect.height + "px";
     central.style.height = window.innerHeight - parseInt(central.style.top) + "px";
 
@@ -46,12 +52,14 @@ function Mainonload() {
     imgRight.style.top = imgLeft.style.top;
     imgRight.style.left = centralRect.left + centralRect.width - 50 - imgRight.clientHeight + "px";
     startPos.style.left = (centralRect.left + centralRect.width - startPos.clientWidth) * 0.5 + "px";
-    startPos.style.top =  centralRect.top + centralRect.height - startPos.clientHeight - 50 + "px";
+    startPos.style.top = centralRect.top + centralRect.height - startPos.clientHeight - 50 + "px";
 
-    startPos.onclick = function(e) {
+    displayBar.style.top = centralRect.top + 50 + "px";
+    displayBar.style.left = centralRect.left + centralRect.width/2 - displayRect.width/2;
+
+    startPos.onclick = function (e) {
         if (recording) return;
 
-        ShowNextPair();
         recording = true;
         coordsTrack = [];
         coordsTrack.push(getMouseLocation(e, central));
@@ -59,14 +67,15 @@ function Mainonload() {
         $("#mouseCoords").addClass("mouseCoordsAnimated");
     };
 
-    imgLeft.onclick = imgRight.onclick = function(e) {
+    imgLeft.onclick = imgRight.onclick = function (e) {
         if (!recording) return;
 
         recording = false;
         var pos = getMouseLocation(e, central);
         //TODO: ADD RATIO AND AREA TO data
         var data = {id: id, username: userName, selection: this.id, coordinates: coordsTrack};
-        $.ajax({url: "/data",
+        $.ajax({
+                url: "/data",
                 type: "POST",
                 dataType: "json",
                 data: JSON.stringify(data),
@@ -144,56 +153,51 @@ function CheckName() {
     }
 }
 
-function ShowNextPair() {
-
-}
-
 function draw() {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-    //picking ratio
-    var ratio = 1 + Math.floor(Math.random()*15) * 1/3;
-    if (Math.random() >= 0.5)  {
-      ratio = 1/ratio;
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        //picking ratio
+        var ratio = 1 + Math.floor(Math.random() * 15) / 3;
+        if (Math.random() >= 0.5) {
+            ratio = 1 / ratio;
+        }
+        // selecting a random area factor
+        var area = [.65, .75, .85, 1.15, 1.25, 1.35];
+        var pickarea = function () {
+            return area[Math.floor(Math.random() * 5)];
+        };
+        //set width and height formulas
+        var w = ratio * Math.sqrt((pickarea * 22500) / ratio);
+        var h = Math.sqrt((pickarea * 22500) / ratio);
+        //draw rectangle
+        ctx.fillRect(500, 200, w, h);
+        //notes: rect(width/2, height/2, ratio*sqrt(area/ratio), sqrt(area/ratio) ); //w**2 * 2 = area => w = sqrt(area/2)
+
     }
-    // selecting a random area factor
-  var area = [.65, .75, .85, 1.15, 1.25, 1.35];
-  var pickarea = function () {
-  var randomarea = area[Math.floor(Math.random()* 5)];
-  return randomarea;
-   };
-   //set width and height formulas
-   var w = ratio*sqrt((pickarea*22500)/ratio);                  
-   var h = sqrt((pickarea*22500)/ratio);
-    //draw rectangle
-    ctx.fillRect(500, 200, w, h);
-    //notes: rect(width/2, height/2, ratio*sqrt(area/ratio), sqrt(area/ratio) ); //w**2 * 2 = area => w = sqrt(area/2)
- 
-  }
 }
 
 function drawtop() {
-    var canvas = document.getElementById('top');
+    var canvas = document.getElementById('displayBar');
     if (canvas.getContext) {
-      var ctx = canvas.getContext('2d'); 
-      var X = canvas.width / 2;
-      var Y = canvas.height / 2;
-      var R = 45;
-      ctx.beginPath();
-      ctx.arc(X - 300, Y, R, 0, 2 * Math.PI, false);
-      ctx.lineWidth = 3;
-      ctx.strokeStyle = '#a9a9a9';
-      ctx.stroke();
-      ctx.font = "20px Arial";
-      ctx.fillText("Smaller",X - 333,Y + 5);
-      ctx.beginPath();
-      ctx.arc(X + 300, Y, R, 0, 2 * Math.PI, false);
-      ctx.lineWidth = 3;
-      ctx.strokeStyle = '#a9a9a9';
-      ctx.stroke();
-      ctx.font = "20px Arial";
-      ctx.fillText("Larger",X + 272,Y + 5);
-      ctx.fillRect(X-65,Y-70,150,150);
+        var ctx = canvas.getContext('2d');
+        var X = canvas.width / 2;
+        var Y = canvas.height / 2;
+        var R = 45;
+        ctx.beginPath();
+        ctx.arc(X - 300, Y, R, 0, 2 * Math.PI, false);
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#a9a9a9';
+        ctx.stroke();
+        ctx.font = "20px Arial";
+        ctx.fillText("Smaller", X - 333, Y + 5);
+        ctx.beginPath();
+        ctx.arc(X + 300, Y, R, 0, 2 * Math.PI, false);
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#a9a9a9';
+        ctx.stroke();
+        ctx.font = "20px Arial";
+        ctx.fillText("Larger", X + 272, Y + 5);
+        ctx.fillRect(X - 65, Y - 70, 150, 150);
     }
 }
